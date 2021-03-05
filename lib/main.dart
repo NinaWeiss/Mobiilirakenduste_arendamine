e93b45e26e5376955f41c7c108e51d49e460f879
NinaWeiss <nweiss1@tlu.ee>
2021-03-05 11:49:41 +0200

    multi pages

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,10 @@ import 'chewie_list_item.dart';
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctxt) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -18,16 +19,122 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class DrawerOnly extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctxt) {
+    return new Drawer(
+    child: new ListView(
+    children:<Widget>[
+    new DrawerHeader(
+    child: new Text("Categories"),
+    decoration: new BoxDecoration(color: Colors.greenAccent),
+    ),
+      new ListTile(
+        title: new Text("Home"),
+        onTap: (){
+          Navigator.pop(ctxt);
+          Navigator.push(ctxt, new MaterialPageRoute(builder: (ctxt) => new MyHomePage()));
+        },
+          selectedTileColor: Colors.amber
+      ),
+    new ListTile(
+    //leading: const Image(Image.add)
+    title: new Text("Puksiirauto Tom"),
+    onTap: (){
+    Navigator.pop(ctxt);
+    Navigator.push(ctxt, new MaterialPageRoute(builder: (ctxt) => new SecondPage()));
+    },
+    ),
+    new ListTile(
+    //leading: const Image(Image.add)
+    title: new Text("CoComelon Baby"),
+    onTap: (){
+    Navigator.pop(ctxt);
+    Navigator.push(ctxt, new MaterialPageRoute(builder: (ctxt) => new CoComelonBabyPage()));
+    },
+    )
+    ],
+    )
+    );
+
+  }
+}
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext ctxt) {
     return Scaffold(
+      drawer: new DrawerOnly(),
       appBar: AppBar(
-        title: Text('Videos'),
+        title: Text('Home'),
       ),
       body: ListView(
         children: <Widget>[
          /* ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'videos/BabyShark.mp4',
+            ),
+            looping: true,
+          ),*/
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.network(
+              'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4',
+            ),
+          ),
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.network(
+              'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Scaffold(
+      drawer: new DrawerOnly(),
+      appBar: AppBar(
+        title: Text('Puksiirauto Tom'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          /* ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'videos/BabyShark.mp4',
+            ),
+            looping: true,
+          ),*/
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.network(
+              'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4',
+            ),
+          ),
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.network(
+              'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CoComelonBabyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Scaffold(
+      drawer: new DrawerOnly(),
+      appBar: AppBar(
+        title: Text('CoComelonBaby'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          /* ChewieListItem(
             videoPlayerController: VideoPlayerController.asset(
               'videos/BabyShark.mp4',
             ),
