be1668aac448a3f1e2dcc1d3a85cb9f2d6ed96c0
NinaWeiss <nweiss1@tlu.ee>
2021-03-12 15:04:49 +0200

    kaamera lisamine + galerie

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 //import 'package:chewie_prep/chewie_list_item.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
 import 'camera_screen.dart';
 import 'chewie_list_item.dart';
@@ -120,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
           controller: _tabController,
               indicatorColor: Colors.white,
               tabs: <Widget>[
-                Tab(icon: Icon(Icons.play_arrow)),
+                Tab(icon: Icon(Icons.home)),
                 Tab(icon: Icon(Icons.camera_alt)),
               ],
         ),
@@ -130,33 +133,102 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
         controller: _tabController,
           children: <Widget>[
             VideoItemList(),
-            CameraScreen(widget.cameras),
+            LandingScreen(),
+            //CameraScreen(widget.cameras),
           ],
       ),
-        /*body: new ListView(
-        children: <Widget>[
-         /* ChewieListItem(
-            videoPlayerController: VideoPlayerController.asset(
-              'videos/BabyShark.mp4',
-            ),
-            looping: true,
-          ),*/
-          ChewieListItem(
-            videoPlayerController: VideoPlayerController.network(
-              'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4',
-            ),
-          ),
-          ChewieListItem(
-            videoPlayerController: VideoPlayerController.network(
-              'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4',
-            ),
-          ),
-        ],
-      ),
-      ),*/
+
     );
   }
 }
+
+class LandingScreen extends StatefulWidget {
+
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  File imageFile;
+
+  _openGallery(BuildContext context) async{
+    // ignore: deprecated_member_use
+    var picture= await ImagePicker.pickImage(source: ImageSource.gallery);
+    this.setState((){
+      imageFile=picture;
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _openCamera(BuildContext context) async{
+    // ignore: deprecated_member_use
+    var picture= await ImagePicker.pickImage(source: ImageSource.camera);
+    this.setState((){
+      imageFile=picture;
+    });
+    Navigator.of(context).pop();
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context){
+    return showDialog(context: context,builder:(BuildContext context){
+      return AlertDialog(
+        title: Text("Make a choice"),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children:<Widget>[
+              GestureDetector(
+                child: Text("Gallery"),
+                onTap: (){
+                  _openGallery(context);
+                },
+              ),
+              Padding(padding:EdgeInsets.all(8.0)),
+              GestureDetector(
+                child: Text("Camera"),
+                onTap: (){
+                  _openCamera(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget _decideImageView(){
+    if(imageFile == null){
+      return Text("No image selected");
+    }else{
+      Image.file(imageFile,width: 400,height: 400);
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar:AppBar(
+      ),
+        body: Container(
+          child: Center(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                _decideImageView(),
+                RaisedButton(onPressed:(){
+                  _showChoiceDialog(context);
+                },child: Text("Select Image"),)
+            ],
+            ),
+          ),
+        ),
+    );
+  }
+}
+
+
+
 
 class VideoItemList extends StatelessWidget {
   @override
