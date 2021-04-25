828d364a878f4d6f6348f621a29a02037614986b
Gevin Genro Paas <55079799+ggpaas@users.noreply.github.com>
2021-04-26 01:47:39 +0300

    Fetch data from the internet

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 //import 'package:chewie_prep/chewie_list_item.dart';
-import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/chewie_list_item.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:video_player/video_player.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:gallery_saver/gallery_saver.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:camera/camera.dart';
+import 'package:flutter_app/chewie_list_item.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:video_player/video_player.dart';
 
 List<CameraDescription> cameras;
 
@@ -19,6 +18,37 @@ Future<Null> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
   runApp(MyApp());
+}
+
+Future<Album> fetchAlbum() async {
+  final response =
+      await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({@required this.userId, @required this.id, @required this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -59,7 +89,7 @@ class DrawerOnly extends StatelessWidget {
               Navigator.pop(ctxt);
               Navigator.push(
                   ctxt,
-                  new MaterialPageRoute(
+                  new MaterialPageRoute<void>(
                       builder: (ctxt) => new MyHomePage(cameras)));
             },
             selectedTileColor: Colors.amber),
@@ -69,8 +99,10 @@ class DrawerOnly extends StatelessWidget {
           trailing: Icon(Icons.keyboard_arrow_right),
           onTap: () {
             Navigator.pop(ctxt);
-            Navigator.push(ctxt,
-                new MaterialPageRoute(builder: (ctxt) => new SettingsPage()));
+            Navigator.push(
+                ctxt,
+                new MaterialPageRoute<void>(
+                    builder: (ctxt) => new SettingsPage()));
           },
         ),
         new ListTile(
@@ -82,8 +114,10 @@ class DrawerOnly extends StatelessWidget {
           title: new Text("Puksiirauto Tom"),
           onTap: () {
             Navigator.pop(ctxt);
-            Navigator.push(ctxt,
-                new MaterialPageRoute(builder: (ctxt) => new SecondPage()));
+            Navigator.push(
+                ctxt,
+                new MaterialPageRoute<void>(
+                    builder: (ctxt) => new SecondPage()));
           },
         ),
         new ListTile(
@@ -97,8 +131,19 @@ class DrawerOnly extends StatelessWidget {
             Navigator.pop(ctxt);
             Navigator.push(
                 ctxt,
-                new MaterialPageRoute(
+                new MaterialPageRoute<void>(
                     builder: (ctxt) => new CoComelonBabyPage()));
+          },
+        ),
+        new ListTile(
+          title: new Text("Data from Internet"),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            Navigator.pop(ctxt);
+            Navigator.push(
+                ctxt,
+                new MaterialPageRoute<void>(
+                    builder: (ctxt) => new FetchdataPage()));
           },
         ),
       ],
@@ -107,7 +152,7 @@ class DrawerOnly extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  var cameras;
+  List<CameraDescription> cameras;
 
   MyHomePage(this.cameras);
 
@@ -170,7 +215,7 @@ class _LandingScreenState extends State<LandingScreen> {
   File imageFile;
   File videoFile;
 
-  _openGallery(BuildContext context) async {
+  void _openGallery(BuildContext context) async {
     var picture = await ImagePicker().getImage(source: ImageSource.gallery);
     var video = await ImagePicker().getVideo(source: ImageSource.gallery);
 
@@ -182,7 +227,7 @@ class _LandingScreenState extends State<LandingScreen> {
     Navigator.of(context).pop();
   }
 
-  _openCamera(BuildContext context) async {
+  void _openCamera(BuildContext context) async {
     // ignore: deprecated_member_use
     var picture = await ImagePicker().getImage(source: ImageSource.camera);
     this.setState(() {
@@ -192,7 +237,7 @@ class _LandingScreenState extends State<LandingScreen> {
     Navigator.of(context).pop();
   }
 
-  _recordVideo(BuildContext context) async {
+  void _recordVideo(BuildContext context) async {
     // ignore: deprecated_member_use
     var video = await ImagePicker().getVideo(source: ImageSource.gallery);
     this.setState(() {
@@ -371,7 +416,6 @@ class CoComelonBabyPage extends StatelessWidget {
   }
 }
 
-
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext ctxt) {
@@ -383,39 +427,84 @@ class SettingsPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-
           TextField(
             decoration: InputDecoration(
-                border: OutlineInputBorder(),
+              border: OutlineInputBorder(),
             ),
             onChanged: (text) {
               this.saveFile(text);
             },
           ),
-
         ],
       ),
     );
   }
+
   Future<String> getFilePath() async {
-    Directory appDocumentsDirectory = await getApplicationDocumentsDirectory(); // 1
+    Directory appDocumentsDirectory =
+        await getApplicationDocumentsDirectory(); // 1
     String appDocumentsPath = appDocumentsDirectory.path; // 2
     String filePath = '$appDocumentsPath/appSettings.txt'; // 3
 
     return filePath;
   }
 
-  void saveFile(txt) async {
+  void saveFile(String txt) async {
     File file = File(await getFilePath()); // 1
     file.writeAsString(txt); // 2
   }
+
   void readFile() async {
     File file = File(await getFilePath()); // 1
     String fileContent = await file.readAsString(); // 2
 
     print('File Content: $fileContent');
   }
-
 }
 
+class FetchdataPage extends StatefulWidget {
+  FetchdataPage({Key key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<FetchdataPage> {
+  Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.title);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
